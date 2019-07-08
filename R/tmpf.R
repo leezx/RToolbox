ef26b2f488aa78c510936b0057b1fb44cb873022
leezx <zxlee@hku.hk>
2019-07-09 00:18:09 +0800

    change

---
+++ R/tmpf.R
@@ -0,0 +1,11 @@
+#' description of the example function
+#' @param param1 param1
+#'
+#' @return the return object
+#' @export
+#' @examples
+#' example code
+#'
+example.function <- function(param1) {
+	NULL
+}
