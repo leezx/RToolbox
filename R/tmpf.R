ca867320096d96a985c9b65d899017a4a76c70fd
Zeki <zxlee@hku.hk>
2019-07-11 12:23:31 +0800

    Update tmpf.R

--- R/tmpf.R
+++ R/tmpf.R
@@ -12,12 +12,13 @@ example.function <- function(param1=NULL) {
 
 #' GO KEGG GSEA analysis
 #' @param geneList a gene list
+#' @param use.score the score used for the GSEA analysis
 #' @param organism organism (hs and mm)
 #'
 #' @return a list with GO and KEGG GSEA annotation result (clusterProfiler format)
 #' @export
 #' @examples
-#' result <- ora.go.kegg.clusterProfiler(geneList = new_moduleList, organism="mm")
+#' gsea_list <- gsea.go.kegg.clusterProfiler(geneList = pheno_DEGs, use.score = "cor", organism="mm")
 #'
 gsea.go.kegg.clusterProfiler <- function(geneList=DEGs_list_full, use.score="cor", organism="hs") {
 	pAdjustMethod = "BH"; pvalueCutoff = 0.05
