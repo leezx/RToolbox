926fa8c57f9cc1723ceeef2fc291275ae59142ca
Zeki <zxlee@hku.hk>
2019-07-11 20:00:11 +0800

    Update Plot.R

--- R/Plot.R
+++ R/Plot.R
@@ -167,8 +167,8 @@ plot.GO.barplot <- function(barplot_df, color="random") {
     axis.text.x = element_text(size = 12, color = "black", face = "plain"),
     axis.title =element_text(size = 15)) +
   # axis.title = element_blank() ,plot.title = element_blank(), axis.ticks.y = element_blank(), 
-    axis.ticks.x = element_blank(), axis.text.x = element_blank(),axis.line = element_blank(),
-    panel.border = element_blank(),
+  # axis.ticks.x = element_blank(), axis.text.x = element_blank(),axis.line = element_blank(),
+  # panel.border = element_blank(),
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),  plot.margin=unit(c(0,0,0,0), "cm"), 
         panel.border = element_blank()) +
   theme(axis.line = element_line(color = 'black')) +
