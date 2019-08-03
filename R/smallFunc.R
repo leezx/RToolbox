3a87b772154294ac6dd3fcf6411883fffea9d740
leezx <zxlee@hku.hk>
2019-08-03 15:53:17 +0800

    change

--- R/smallFunc.R
+++
@@ -1,9 +0,0 @@
-# repeat the col or row for n times
-# example: rep.df <- rep.col(all_tsne[,cluster], length(groups))
-rep.row<-function(x,n){
-   matrix(rep(x,each=n),nrow=n)
-}
-rep.col<-function(x,n){
-   matrix(rep(x,each=n), ncol=n, byrow=TRUE)
-}
-
