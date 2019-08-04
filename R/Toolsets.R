32a605559d6e30a3774bae16df4df606a3c8c7c4
Zeki <zxlee@hku.hk>
2019-08-05 00:26:32 +0800

    Update Toolsets.R

--- R/Toolsets.R
+++ R/Toolsets.R
@@ -192,6 +192,48 @@ pre.scale.data <- function(raw.data=logcounts(sce_HSCR), max.value=2) {
 
 #' prepare the annotation for the pheatmap
 #' @param sce sce object
+#' @param FeatureList FeatureList
+#' @param sortBy sortBy
+#' @param colors colors
+#'
+#' @return anno list
+#' @export
+#' @examples
+#' group.order <- c("Control", "S-HSCR", "L-HSCR")
+#' FeatureList <- list("Sample"=sample.order[1:8], "Group"=group.order)
+#' anno <- pre.pheatmap.anno.general(sce_HSCR_c1, FeatureList, sortBy = "Sample")
+#'
+pre.pheatmap.anno.general <- function(sce=sce, FeatureList=FeatureList, sortBy="impute_cluster", 
+                                      colors=list("1"=brewer.pal(8,"Set2"), "2"=brewer.pal(12,"Set3"))) {
+    # group.order=group.order, sample.order=sample.order, Cluster="impute_cluster", Sample="cellGroup"
+    anno <- list()
+    annotation_colors <- FeatureList
+    annotation_col <- as.data.frame(colData(sce)[,c("cellName", names(FeatureList))])
+    for (j in 1:length(names(FeatureList))) {
+        # print(j)
+        i <- names(FeatureList)[j]
+        # print(i)
+        tmpNames <- FeatureList[[i]]
+        # print(tmpNames)
+        annotation_col[,i] <- factor(annotation_col[,i], levels = tmpNames)
+        tmpcolor <- colors[[j]][1:length(tmpNames)] # mistake [] and [[]]
+        if (length(tmpNames)>8) {
+            tmpcolor <- brewer.pal(12,"Set3")[1:length(tmpNames)]
+        }
+        names(tmpcolor) <- tmpNames
+        # print(annotation_colors)
+        annotation_colors[[i]]  <- tmpcolor
+    }
+    annotation_col <- annotation_col[order(annotation_col[,sortBy], decreasing = F),]
+    annotation_col$cellName <- NULL
+    #######
+    anno[["col"]] <- annotation_col
+    anno[["colors"]] <- annotation_colors
+    return(anno)
+}
+
+#' prepare the annotation for the pheatmap
+#' @param sce sce object
 #' @param group.order group.order
 #' @param sample.order sample.order
 #' @param Cluster the feature take as Cluster
